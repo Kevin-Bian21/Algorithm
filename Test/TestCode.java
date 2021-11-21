42f62fe7d4aa4c653e12f20600db0336b7c450cb
BianWenKai <1467295546@qq.com>
2021-11-21 16:30:34 +0800

    '2'的ASCII码值为50

---
+++ Test/TestCode.java
@@ -0,0 +1,17 @@
+package Test;
+
+import org.junit.Test;
+
+/**
+ * @author BianWenKai
+ * @DATE 2021/11/21 - 16:20
+ **/
+public class TestCode {
+    public static void main(String[] args) {
+        System.out.println('A');
+    }
+    @Test
+    public void test(){
+        System.out.println('2'+1);
+    }
+}
