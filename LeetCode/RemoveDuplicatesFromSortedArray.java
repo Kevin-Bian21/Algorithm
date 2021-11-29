401593d390f4efe8a5b5ba154543e2903e45413a
BianWenKai <1467295546@qq.com>
2021-11-29 18:23:58 +0800

    降低时间复杂度

--- LeetCode/RemoveDuplicatesFromSortedArray.java
+++ LeetCode/RemoveDuplicatesFromSortedArray.java
@@ -5,6 +5,12 @@ import java.util.Arrays;
 /**
  * @author BianWenKai
  * @DATE 2021/11/29 - 17:28
+ *
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
  **/
 public class RemoveDuplicatesFromSortedArray {
 
@@ -14,19 +20,16 @@ public class RemoveDuplicatesFromSortedArray {
     }
 
     public static int[] removeDuplicates(int[] nums) {
-        int j = 0;
-        int len = nums.length;
-        while (j < len - 1) {
-            if (nums[j] == nums[j + 1]) {
-                if (len - 1 - j >= 0)
-                    System.arraycopy(nums, j + 1, nums, j, len - 1 - j);
-                System.out.println(len);
-                len --;
-                continue;
+        if (nums == null || nums.length <= 0)
+            return nums;
+        int index = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[index]) {
+                index ++;
+                nums[index] = nums[i];
             }
-            j++;
         }
-        System.out.println(len);
-        return nums ;
+        System.out.println(index);
+        return nums;
     }
 }
